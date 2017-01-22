014eeb3de82c92292c515c0081c34ea465403fb0
George Spake <gpspake@gmail.com>
2017-01-22 17:29:34 -0600

    create page title component

--- react-only/js/ClientApp.js
+++ react-only/js/ClientApp.js
@@ -1,11 +1,21 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var PageTitle = React.createClass({
+	render: function () {
+		return (
+			div(null,
+				h1(null, 'Page Title')
+			)
+		)
+	}
+})
+
 var FirstComponent = React.createClass({
 	render: function () {
 		return (
 			div(null,
-				h1(null, 'FirstComponent')
+				React.createElement(PageTitle)
 			)
 		)
 	}
