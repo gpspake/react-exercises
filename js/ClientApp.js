6fee2a3af781cb3db7d4875a02d632f600d3d1ac
George Spake <gpspake@gmail.com>
2017-01-23 17:19:50 -0600

    refactor app component with react router and landing component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { HashRouter, Match } from 'react-router'
+import Landing from './Landing'
 import '../public/normalize.css'
 import '../public/style.css'
 
@@ -7,11 +9,9 @@ const App = React.createClass({
   render () {
     return (
       <div className='app'>
-        <div className='landing'>
-          <h1>React Video</h1>
-          <input type='text' placeholder='Search' />
-          <a>or Browse All</a>
-        </div>
+        <HashRouter>
+          <Match exactly pattern='/' component={Landing} />
+        </HashRouter>
       </div>
     )
   }
