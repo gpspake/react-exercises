cae787bcd8fc7b159c708102e24d0b4430965127
George Spake <gpspake@gmail.com>
2017-01-22 18:17:56 -0600

    refactor for standard

--- react-only/js/ClientApp.js
+++ react-only/js/ClientApp.js
@@ -1,29 +1,31 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var PageTitle = React.createClass({
-	render: function () {
-		return (
-			div(null,
-				h1({style: {color: this.props.color, fontWeight: '300'}}, this.props.title)
-			)
-		)
-	}
+  render: function () {
+    return (
+      div(null,
+        h1({style: {color: this.props.color, fontWeight: '300'}}, this.props.title)
+      )
+    )
+  }
 })
 
 var PageTitleFactory = React.createFactory(PageTitle)
 
 var FirstComponent = React.createClass({
-	render: function () {
-		return (
-			div(null,
-				PageTitleFactory({title: 'Page Title 1', color: 'peru'}),
-				PageTitleFactory({title: 'Page Title 2', color: 'tomato'}),
-				PageTitleFactory({title: 'Page Title 3', color: 'rebeccapurple'}),
-				PageTitleFactory({title: 'Page Title 4', color: 'mediumaquamarine'})
-			)
-		)
-	}
+  render: function () {
+    return (
+      div(null,
+        PageTitleFactory({title: 'Page Title 1', color: 'peru'}),
+        PageTitleFactory({title: 'Page Title 2', color: 'tomato'}),
+        PageTitleFactory({title: 'Page Title 3', color: 'rebeccapurple'}),
+        PageTitleFactory({title: 'Page Title 4', color: 'mediumaquamarine'})
+      )
+    )
+  }
 })
 
 ReactDOM.render(React.createElement(FirstComponent), document.getElementById('app'))
