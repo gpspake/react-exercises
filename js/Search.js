b3880c4c79bf422082d64b8b28425011753b1eaf
George Spake <gpspake@gmail.com>
2017-01-23 19:05:29 -0600

    render show titles in components

--- js/Search.js
+++ js/Search.js
@@ -5,7 +5,11 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+        {preload.shows.map((function (show) {
+          return (
+            <h3>{show.title}</h3>
+          )
+        }))}
       </div>
     )
   }
