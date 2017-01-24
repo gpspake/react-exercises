cd56b6f74d679ced9f19607cf708a858098bdb3e
George Spake <gpspake@gmail.com>
2017-01-23 22:42:29 -0600

    add show details to search component

--- js/Search.js
+++ js/Search.js
@@ -5,11 +5,18 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((function (show) {
+        {preload.shows.map((show) => {
           return (
-            <h3>{show.title}</h3>
+            <div className='show-card'>
+              <img src={`/public/img/posters/${show.poster}`} />
+              <div>
+                <h3>{show.title}</h3>
+                <h4>({show.year})</h4>
+                <p>{show.description}</p>
+              </div>
+            </div>
           )
-        }))}
+        })}
       </div>
     )
   }
