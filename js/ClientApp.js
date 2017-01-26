e164cfb8ed74d8ed69a7521bc7406afba141d4da
George Spake <gpspake@gmail.com>
2017-01-25 20:32:20 -0600

    create route for details

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Search from './Search'
+import Details from './Details'
 import '../public/normalize.css'
 import '../public/style.css'
 
@@ -13,6 +14,7 @@ const App = React.createClass({
         <div className='app'>
           <Match exactly pattern='/' component={Landing} />
           <Match exactly pattern='/search' component={Search} />
+          <Match pattern='/details/:id' component={Details} />
         </div>
       </BrowserRouter>
     )
