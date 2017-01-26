13726337eb212414b337d4c1e89b1a71fddbbee4
George Spake <gpspake@gmail.com>
2017-01-25 19:29:25 -0600

    add test search should render correct amount of shows based on search

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -16,3 +16,12 @@ test('Search should render a ShowCard for each show', () => {
   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
 
+test('Search should render correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change', {target: {value: searchWord}})
+  const showCount = preload.shows.filter((show) => {
+      return `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+    }).length
+  expect(component.find(ShowCard).length).toEqual(showCount)
+})
