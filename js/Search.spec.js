3db9a6e3e145d64b47c91a3d525c7554ef734f67
George Spake <gpspake@gmail.com>
2017-01-25 18:46:45 -0600

    update search component test to use enzyme

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Search from './Search'
-import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
 
 test('Search snapshot test', () => {
-  const component = renderer.create(<Search/>)
-  const tree = component.toJSON()
+  const component = shallow(<Search/>)
+  const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
 })
