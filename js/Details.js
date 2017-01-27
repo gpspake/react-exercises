0955ebd1712eff9fe349ee16f0b55e6e7799834e
George Spake <gpspake@gmail.com>
2017-01-27 11:43:51 -0600

    refactor imdb data to use redux
    instead of handling the imdb call in the component, pass the imdb id to the getOmdbDetails action creator and dispatch to redux.
    This allows the component to check for imdb data on componentDidMount to avoid making multiple calls each time it is loaded

--- js/Details.js
+++ js/Details.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import axios from 'axios'
 import Header from './Header'
-const {shape, string} = React.PropTypes
+import { connect } from 'react-redux'
+import { getOmdbDetails } from './actionCreators'
+const { shape, string, func } = React.PropTypes
 
 const Details = React.createClass({
   propTypes: {
@@ -12,20 +13,22 @@ const Details = React.createClass({
       trailer: string,
       description: string,
       imdbID: string
-    })
+    }),
+    omdbData: shape({
+      imdbID: string
+    }),
+    dispatch: func
   },
   componentDidMount () {
-    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
-      .then((response) => {
-        this.setState({omdbData: response.data})
-      })
-      .catch((error) => console.error('axios error', error))
+    if (!this.props.omdbData.imdbRating) {
+      this.props.dispatch(getOmdbDetails(this.props.show.imdbID))
+    }
   },
   render () {
     const {title, description, year, poster, trailer} = this.props.show
     let rating
-    if (this.state.omdbData.imdbRating) {
-      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    if (this.props.omdbData.imdbRating) {
+      rating = <h3>{this.props.omdbData.imdbRating}</h3>
     } else {
       rating = <img src='/public/img/loading.png' />
     }
@@ -50,4 +53,11 @@ const Details = React.createClass({
   }
 })
 
-export default Details
+const mapStateToProps = (state, ownProps) => {
+  const omdbData = state.omdbData[ownProps.show.imdbID] ? state.omdbData[ownProps.show.imdbID] : {}
+  return {
+    omdbData
+  }
+}
+
+export default connect(mapStateToProps)(Details)
