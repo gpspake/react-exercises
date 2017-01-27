65be08226f4cc5d29214733cbd4621a84d141d30
George Spake <gpspake@gmail.com>
2017-01-27 11:29:27 -0600

    add reducer for add omdb data

--- js/reducers.js
+++ js/reducers.js
@@ -1,7 +1,8 @@
-import { SET_SEARCH_TERM } from './actions'
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
 
 const DEFAULT_STATE = {
-  searchTerm: ''
+  searchTerm: '',
+  omdbData: {}
 }
 
 const setSearchTerm = (state, action) => {
@@ -10,10 +11,20 @@ const setSearchTerm = (state, action) => {
   return newState
 }
 
+const addOmdbData = (state, action) => {
+  const newOmdbData = {}
+  Object.assign(newOmdbData, state.newOmdbData, {[action.imdbID]: action.omdbData})
+  const newState = {}
+  Object.assign(newState, state, {omdbData: newOmdbData})
+  return newState
+}
+
 const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case SET_SEARCH_TERM:
       return setSearchTerm(state, action)
+    case ADD_OMDB_DATA:
+      return addOmdbData(state, action)
     default:
       return state
   }
