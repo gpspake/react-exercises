94322f3507cfd05f71b2a12ec1a4745c15e0fd9b
George Spake <gpspake@gmail.com>
2017-01-26 23:25:33 -0600

    connect landing search to store
    mapStateToProps accepts state and passes the part of state that is needed to props

--- js/Landing.js
+++ js/Landing.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
+import { setSearchTerm } from './actionCreators'
+const { string, func } = React.PropTypes
 
 const Landing = React.createClass({
+  propTypes: {
+    searchTerm: string,
+    dispatch: func
+  },
+  handleSearchTermChange (event) {
+    this.props.dispatch(setSearchTerm(event.target.value))
+  },
   render () {
     return (
       <div className='landing'>
         <h1>React Video</h1>
-        <input type='text' placeholder='Search' />
+        <input
+          onChange={this.handleSearchTermChange}
+          value={this.props.searchTerm}
+          type='text'
+          placeholder='Search' />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
   }
 })
 
-export default Landing
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Landing)
