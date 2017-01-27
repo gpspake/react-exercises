596c3e9e5b8bfd5748817d98b7fc5d6714af8061
George Spake <gpspake@gmail.com>
2017-01-27 11:24:13 -0600

    apply thunk middleware to store
    applying middleware allows redux to undersand things other than just actions - like promises,  observables, and thunks - bh

--- js/store.js
+++ js/store.js
@@ -1,7 +1,9 @@
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk), //now redux has the ability to understand functions instead of actions
   typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? devToolsExtension() : (f) => f
 ))
 
