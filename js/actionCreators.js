4de9ab1818b465741b1a30b46d6534e44f7a8156
George Spake <gpspake@gmail.com>
2017-01-27 11:32:41 -0600

    add action creator for add omdb data
    getOmdbDetails is going to be our thunk creator, it will return a function
    the function (thunk) returned by getOmdbDetails gets called by redux and dispatches the action when the promise returns a response

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,5 +1,20 @@
-import { SET_SEARCH_TERM } from './actions'
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
+import axios from 'axios'
 
 export function setSearchTerm (searchTerm) {
   return { type: SET_SEARCH_TERM, searchTerm }
+}
+
+export function addOmdbData (imdbID, omdbData) {
+  return { type: ADD_OMDB_DATA, imdbID, omdbData }
+}
+
+export function getOmdbDetails (imdbID) {
+  return function (dispatch, getState) {
+    axios.get(`http://www.omdbapi.com/?i=${imdbID}`)
+      .then((response) => {
+        dispatch(addOmdbData(imdbID, response.data))
+      })
+      .catch((error) => console.error('axios error', error))
+  }
 }
