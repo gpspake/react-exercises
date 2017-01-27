dd07b9e5d89804455e437ad36f5a33ef11681fad
George Spake <gpspake@gmail.com>
2017-01-26 23:54:20 -0600

    route search submit to search page
    wrap input in form to handle submit
    context is global so should treated as read only
    react router v4 uses context to make the router available globally
    context types should be declared at the top of the component

--- js/Landing.js
+++ js/Landing.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { setSearchTerm } from './actionCreators'
-const { string, func } = React.PropTypes
+const { string, func, object } = React.PropTypes
 
 const Landing = React.createClass({
+  contextTypes: {
+    router: object
+  },
   propTypes: {
     searchTerm: string,
     dispatchSetSearchTerm: func
@@ -12,15 +15,21 @@ const Landing = React.createClass({
   handleSearchTermChange (event) {
     this.props.dispatchSetSearchTerm(event.target.value)
   },
+  handleSearchSubmit (event) {
+    event.preventDefault()
+    this.context.router.transitionTo('/search')
+  },
   render () {
     return (
       <div className='landing'>
         <h1>React Video</h1>
-        <input
-          onChange={this.handleSearchTermChange}
-          value={this.props.searchTerm}
-          type='text'
-          placeholder='Search' />
+        <form onSubmit={this.handleSearchSubmit} >
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.props.searchTerm}
+            type='text'
+            placeholder='Search' />
+        </form>
         <Link to='/search'>or Browse All</Link>
       </div>
     )
